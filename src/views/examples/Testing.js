f6ba51bd12d3d1c0ccf39624370192e68e54fefc
지성 최 <jisung7278@naver.com>
2022-10-13 22:11:42 +0900

    testing_ver.3

--- src/views/examples/Testing.js
+++ src/views/examples/Testing.js
@@ -1,6 +1,9 @@
+import React from "react";
 import Header from "../../components/Headers/Header";
 import "../../assets/css/cssTesting.css";
+import "../../components/Paginations.js"
 import {Container} from "reactstrap";
+import Paginations from "../../components/Paginations";
 
 const Testing = () =>{
     return(
@@ -10,15 +13,23 @@ const Testing = () =>{
                 <div className={"reservation-main"}>
                     <dic className={"reservation-main-contents"}>
                         <div className={"innerFacility-img"}>
+                            {/* innerFacility-img here */}
+                            <img src={require('../../assets/img/brand/argon-react.png')} alt={"img test"}/>
                             <h1>innerFacility-img</h1>
                         </div>
                         <div className={"innerFacility-seat-time-list"}>
                             <div className={"innerFacility-seat innerFacility-time"}>
                                 <h1>innerFacility-seat innerFacility-time</h1>
+                                <ul className={"reservation-time-select"}>
+                                    reservation-time-select
+                                    <li className={"reservation-seat-select"}>
+                                        reservation-seat-select
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                         <div className={"innerFacility-select"}>
-                            <h1>innerFacility-select</h1>
+                            <Paginations/>
                         </div>
                     </dic>
                 </div>
