fc6d411022f0d8631fc10438f4da9efc4f6850c3
지성 최 <jisung7278@naver.com>
2022-10-14 01:53:12 +0900

    set "innerFacility-img" vertical center

--- src/views/examples/Testing.js
+++ src/views/examples/Testing.js
@@ -13,16 +13,22 @@ const Testing = () =>{
                 <div className={"reservation-main"}>
                     <dic className={"reservation-main-contents"}>
                         <div className={"innerFacility-img"}>
-                            {/* innerFacility-img here */}
+                            {/* innerFacility-img part */}
                             {/* <img src={require('../../assets/img/brand/argon-react.png')} alt={"img test"}/> */}
                             <img src={require('../../assets/img/brand/'+'argon-react.png')} alt={"img test"}/>
-                            <h1>innerFacility-img</h1>
+                            {/*<h1>innerFacility-img</h1>*/}
+                        </div>
+                        <div className={"innerFacility-name"}>
+                            <h1>innerFacility-name</h1>
                         </div>
                         <div className={"innerFacility-seat-time-list"}>
+                            {/* innerFacility time & seat list container part */}
                             <div className={"innerFacility-seat innerFacility-time"}>
                                 <h1>innerFacility-seat innerFacility-time</h1>
+                                {/* innerFacility time list part */}
                                 <ul className={"reservation-time-select"}>
                                     reservation-time-select
+                                    {/* innerFacility seat list part */}
                                     <li className={"reservation-seat-select"}>
                                         reservation-seat-select
                                     </li>
@@ -30,6 +36,7 @@ const Testing = () =>{
                             </div>
                         </div>
                         <div className={"innerFacility-select"}>
+                            {/* innerFacility change part */}
                             <Paginations/>
                         </div>
                     </dic>
