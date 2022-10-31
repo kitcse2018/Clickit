e37a2c5ca4ef86a47d0e9177c3045121b79344e8
지성 최 <jisung7278@naver.com>
2022-10-31 16:37:20 +0900

    create UserRoute.js

--- src/views/examples/Login.js
+++ src/views/examples/Login.js
@@ -35,17 +35,16 @@ import "../../assets/css/loginView.css";
 //import {useUserContext} from "../../methods/loginMethods";
 //import {useHistory} from "react-router-dom";
 import {useState} from "react";
-import { Redirect } from "react-router-dom";
-import LoginForm from "./LoginForm";
+import {signIn} from "../../methods/signIn";
 
-import { signIn } from '../../methods/signIn';
 const Login = () => {
+  const password_design = "비밀번호 구성\n원스톱 이메일 앞 3자리 + 전화번호 뒷 4자리";
+
   const [user, setUser] = useState(null);
-  const login = ({ id, password }) => setUser(signIn({ id, password }));
-  const authenticated = user== null ? null : user.authenticated;
-  const logout = () => setUser(null);
+  const authenticated = user != null;
 
-
+  const mLogin = ({ id, password }) => setUser(signIn({id, password}));
+  const mLogout = () => setUser(null);
 
   // start
 
@@ -77,9 +76,134 @@ const Login = () => {
 
   //end
   return (
-      <>
-       <LoginForm login={login} authenticated={authenticated}></LoginForm>
-      </>
+    <>
+      <Col lg="5" md="7">
+        <Card className="bg-secondary shadow border-0">
+          {/* from here */}
+          {/*<CardHeader className="bg-transparent pb-5">*/}
+          {/*  <div className="text-muted text-center mt-2 mb-3">*/}
+          {/*    <small>Sign in with</small>*/}
+          {/*  </div>*/}
+          {/*  <div className="btn-wrapper text-center">*/}
+          {/*    <Button*/}
+          {/*      className="btn-neutral btn-icon"*/}
+          {/*      color="default"*/}
+          {/*      href="#pablo"*/}
+          {/*      onClick={(e) => e.preventDefault()}*/}
+          {/*    >*/}
+          {/*      <span className="btn-inner--icon">*/}
+          {/*        <img*/}
+          {/*          alt="..."*/}
+          {/*          src={*/}
+          {/*            require("../../assets/img/icons/common/github.svg")*/}
+          {/*              .default*/}
+          {/*          }*/}
+          {/*        />*/}
+          {/*      </span>*/}
+          {/*      <span className="btn-inner--text">Github</span>*/}
+          {/*    </Button>*/}
+          {/*    <Button*/}
+          {/*      className="btn-neutral btn-icon"*/}
+          {/*      color="default"*/}
+          {/*      href="#pablo"*/}
+          {/*      onClick={(e) => e.preventDefault()}*/}
+          {/*    >*/}
+          {/*      <span className="btn-inner--icon">*/}
+          {/*        <img*/}
+          {/*          alt="..."*/}
+          {/*          src={*/}
+          {/*            require("../../assets/img/icons/common/google.svg")*/}
+          {/*              .default*/}
+          {/*          }*/}
+          {/*        />*/}
+          {/*      </span>*/}
+          {/*      <span className="btn-inner--text">Google</span>*/}
+          {/*    </Button>*/}
+          {/*  </div>*/}
+          {/*</CardHeader>*/}
+          {/* to here */}
+          <CardBody className="px-lg-5 py-lg-5">
+            <div className="text-center text-muted mb-4">
+              {/*<small>Or sign in with credentials</small>*/}
+            {/*  here */}
+            </div>
+            <Form role="form">
+              <FormGroup className="mb-3">
+                <InputGroup className="input-group-alternative">
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="ni ni-email-83" />
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    placeholder="Student Number"
+                    type="email"
+                    autoComplete="new-email"
+                  />
+                </InputGroup>
+              </FormGroup>
+              <FormGroup>
+                <InputGroup className="input-group-alternative">
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="ni ni-lock-circle-open" />
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <Input
+                    placeholder="Password"
+                    type="password"
+                    autoComplete="new-password"
+                  />
+                </InputGroup>
+              </FormGroup>
+              <div className="custom-control custom-control-alternative custom-checkbox">
+                <input
+                  className="custom-control-input"
+                  id=" customCheckLogin"
+                  type="checkbox"
+                />
+                <label
+                  className="custom-control-label"
+                  htmlFor=" customCheckLogin"
+                >
+                  <span className="text-muted">Remember me</span>
+                </label>
+              </div>
+              <div className="text-center">
+                <Button className="my-4" color="primary" type="button">
+                  Sign in
+                </Button>
+              </div>
+            </Form>
+            <div className={"password-explain"}>
+              <p>{password_design}</p>
+            </div>
+          </CardBody>
+        </Card>
+        <Row className="mt-3">
+          <Col xs="6">
+            <a
+              className="text-light"
+              // href="#pablo"
+              href={"#"}
+              onClick={(e) => e.preventDefault()}
+            >
+              <small>Forgot password?</small>
+            </a>
+          </Col>
+          <Col className="text-right" xs="6">
+            <a
+              className="text-light"
+              // href="#pablo"
+              href={"#"}
+              onClick={(e) => e.preventDefault()}
+            >
+              <small>Create new account</small>
+            </a>
+          </Col>
+        </Row>
+      </Col>
+    </>
   );
 };
 
