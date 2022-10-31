a6dbc307ad1200eab175d87a5a8e0d11a99ab7fc
sonjeongdong <sjd0219@gmail.com>
2022-10-31 22:19:01 +0900

    로그인된당!

--- src/views/examples/Login.js
+++ src/views/examples/Login.js
@@ -34,20 +34,38 @@ import {
 import "../../assets/css/loginView.css";
 //import {useUserContext} from "../../methods/loginMethods";
 //import {useHistory} from "react-router-dom";
-import {useState} from "react";
-import { Redirect } from "react-router-dom";
-import LoginForm from "./LoginForm";
 
-import { signIn } from '../../methods/signIn';
+import LoginForm from "./LoginForm";
+import {useState} from "react";
+import Axios from "axios";
+import {Redirect} from "react-router-dom";
+
+
+
 const Login = () => {
-  const [user, setUser] = useState(null);
-  const login = ({ id, password }) => setUser(signIn({ id, password }));
-  const authenticated = user== null ? null : user.authenticated;
+  const [user, setUser] = useState([]);
+
+  function signIn({id, password}) {
+    Axios.get("http://localhost:3001/signIn", {
+      params: {
+        postAuthId: id, postAuthPassword: password
+      }
+    }).then((response) => {
+      if(response.data[0]==null){alert("Failed to login")}
+      else setUser(response.data[0])
+    });
+  }
+
+  const login = ({ id, password }) => signIn({ id, password });
+  const admin_type = user == null ? null : user.admin_type;
   const logout = () => setUser(null);
+
+  if (admin_type==="푸름관리자") return <Redirect to="/admin/index"/>;
+  if (admin_type==="오름관리자") return <Redirect to="/admin/user-profile"/>;
 
   return (
       <>
-       <LoginForm login={login} authenticated={authenticated}></LoginForm>
+       <LoginForm login={login} admin_type={admin_type}></LoginForm>
       </>
   );
 };
